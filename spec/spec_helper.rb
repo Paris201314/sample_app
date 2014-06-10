e8709f05a918c63cf12ceb44b59d11912a2b3545
Paris201314 <xiep@vmware.com>
2014-06-10 10:49:16 +0800

    Finish static pages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,14 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
+
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+
+require 'capybara/rails'
+require 'capybara/rspec'
+
 require 'rspec/autorun'
+
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -13,6 +19,7 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
 RSpec.configure do |config|
+
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
@@ -38,5 +45,8 @@ RSpec.configure do |config|
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
   #     --seed 1234
-  config.order = "random"
+ 
+ config.order = "random"
+ 
+ config.include Capybara::DSL
 end
